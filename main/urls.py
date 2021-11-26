8571ceee4b0aaae1a7fa94f6f26ad4d2d089835f
R0ixy <nikita6kom@gmail.com>
2021-11-26 12:13:28 +0200

    url fix x2

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     # path('<args>', views.index_with_par, name='index'),
     path('<dark>', views.changemode),
 
