83e4f95480c0a0a2492a723a6021ddc90083bc7e
R0ixy <nikita6kom@gmail.com>
2021-11-26 12:07:12 +0200

    db fix x2

--- DjangoPz/settings.py
+++ DjangoPz/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'DjangoPz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
