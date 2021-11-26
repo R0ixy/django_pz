965597e9638eee261195434a44086efaac574fc6
R0ixy <nikita6kom@gmail.com>
2021-11-26 12:19:40 +0200

    db fix

--- DjangoPz/settings.py
+++ DjangoPz/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!c^$04c+$#j3+fi5b$(48ivoz1yepbxk8q+2ei&a9)6w@bq!0j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -74,8 +74,8 @@ WSGI_APPLICATION = 'DjangoPz.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 import dj_database_url
